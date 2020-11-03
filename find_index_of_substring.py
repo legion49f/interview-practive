da8c21c68c29a15ec854de8823d4a3a02b3d650f
Larus <pabloechegorri@gmail.com>
2020-11-03 14:33:07 -0500

    added new question

---
+++ find_index_of_substring.py
@@ -0,0 +1,25 @@
+def find_index(source:str, target:str):
+    ret_idx = 0
+    count = 0
+    for idx in range(len(source)):
+        if source[idx] == target[0]:
+            if len(target) == 1:
+                return idx
+            ret_idx = idx
+            temp = idx + 1
+            for char in target[1:]:
+                if char == source[temp]:
+                    temp+=1
+                    count+=1
+                else:
+                    ret_idx = 0
+                    break
+                if count == len(target[1:]):
+                    return ret_idx
+    if ret_idx == 0:
+        return -1
+
+print(find_index('pablo echegorri', 'eche'), )
+print(find_index('pablo echegorri', 'i'))
+# print(find_index('pablo echegorri', 'eche'))
+# print(find_index('pablo echegorri', 'eche'))
